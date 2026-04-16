5bcf9c24cdc4b2c27ebfcff0b5047df3874803ea
nityamore <morenityaa@gmail.com>
2026-04-16 18:55:13 +0530

    programs on singly linked list

---
+++ OOP/program15.py
@@ -0,0 +1,117 @@
+class Node:
+    def __init__(self, value):
+        self.data = value
+        self.next = None
+
+class SinglyLL:
+
+    def __init__(self):
+        self.first = None
+        self.iCount = 0
+
+    def InsertFirst(self,no):
+        newn = Node(no)
+
+        # LL is empty
+        if self.first == None:
+            self.first = newn
+        # it contains atleast one node
+        else:
+            newn.next = self.first
+            self.first = newn
+
+        self.iCount = self.iCount + 1
+
+    def InsertLast(self,no):
+        newn = Node(no)
+
+        # LL is empty
+        if self.first == None:
+            self.first = newn
+        # it contains atleast one node
+        else:
+            temp = self.first
+            while(temp.next != None):
+                temp = temp.next
+
+            temp.next = newn
+
+        self.iCount = self.iCount + 1
+
+    def InsertAtPos(self,no,pos):
+        if(pos < 1 or pos > (self.iCount+1)):
+            print("invalid position")
+            return
+
+        if(pos == 1):
+            self.InsertionFirst(no)
+            return
+        elif(pos == self.iCount+1):
+            self.InsertFirst(no)
+            return
+        else:
+            newn = Node(no)
+            temp = self.first
+
+            for i in range(1, pos-1):
+                temp = temp.next
+
+            newn.next = temp.next
+            temp.next = newn
+
+            self.iCount = self.iCount+1
+
+
+    def DeleteFirst(self):
+        pass
+
+    def DeleteLast(self):
+        pass
+
+    def DeleteAtPos(self,pos):
+        pass
+
+
+    def Display(self):
+        temp = self.first
+
+        while(temp != None):
+            print("| ",temp.data," |->",end=" ")
+            temp = temp.next
+
+        print("None")
+
+    def Count(self):
+        return self.iCount
+
+def main():
+    sobj = SinglyLL()
+
+    sobj.InsertFirst(101)
+    sobj.InsertFirst(51)
+    sobj.InsertFirst(21)
+    sobj.InsertFirst(11)
+
+    print("Elements of LinkedList are :")
+    sobj.Display()
+
+    print("Number of elements in LinkedList are : ",sobj.Count())
+
+    sobj.InsertLast(111)
+    sobj.InsertLast(121)
+
+    print("Elements of LinkedList are :")
+    sobj.Display()
+
+    print("Number of elements in LinkedList are : ",sobj.Count())
+
+    sobj.InsertAtPos(75,4)
+
+    print("Elements of LinkedList are :")
+    sobj.Display()
+
+    print("Number of elements in LinkedList are : ",sobj.Count())
+
+if __name__ == "__main__":
+    main()
+
---
+++ OOP/program18.py
@@ -0,0 +1,183 @@
+class Node:
+    def __init__(self, value):
+        self.data = value
+        self.next = None
+
+class SinglyLL:
+
+    def __init__(self):
+        self.first = None
+        self.iCount = 0
+
+    def InsertFirst(self,no):
+        newn = Node(no)
+
+        # LL is empty
+        if self.first == None:
+            self.first = newn
+        # it contains atleast one node
+        else:
+            newn.next = self.first
+            self.first = newn
+
+        self.iCount = self.iCount + 1
+
+    def InsertLast(self,no):
+        newn = Node(no)
+
+        # LL is empty
+        if self.first == None:
+            self.first = newn
+        # it contains atleast one node
+        else:
+            temp = self.first
+            while(temp.next != None):
+                temp = temp.next
+
+            temp.next = newn
+
+        self.iCount = self.iCount + 1
+
+    def InsertAtPos(self,no,pos):
+        if(pos < 1 or pos > (self.iCount+1)):
+            print("invalid position")
+            return
+
+        if(pos == 1):
+            self.InsertionFirst(no)
+            return
+        elif(pos == self.iCount+1):
+            self.InsertFirst(no)
+            return
+        else:
+            newn = Node(no)
+            temp = self.first
+
+            for i in range(1, pos-1):
+                temp = temp.next
+
+            newn.next = temp.next
+            temp.next = newn
+
+            self.iCount = self.iCount+1
+
+
+    def DeleteFirst(self):
+        if(self.first == None):
+            return
+
+        temp = self.first
+
+        self.first = self.first.next
+
+        del temp
+
+        self.iCount = self.iCount - 1
+
+    def DeleteLast(self):
+        if(self.first == None):
+            return
+
+        if(self.first.next == None):
+            del self.first
+            self.first = None
+            self.iCount = 0
+
+        else:
+            temp = self.first
+
+        while(temp.next.next != None):
+            temp = temp.next
+
+        del temp.next
+        temp.next = None
+
+        self.iCount = self.iCount - 1
+
+    def DeleteAtPos(self,pos):
+        if(pos < 1 or pos > (self.iCount)):
+            print("Invalid Position")
+            return
+
+        if(pos == 1):
+            self.DeleteFirst()
+            return
+        elif(pos == self.iCount):
+            self.DeleteLast()
+            return
+        else:
+            temp = self.first
+
+            for i in range(1, pos-1):
+                temp = temp.next
+
+            temp.next = temp.next.next
+
+            self.iCount = self.iCount - 1
+
+
+    def Display(self):
+        temp = self.first
+
+        while(temp != None):
+            print("| ",temp.data," |->",end=" ")
+            temp = temp.next
+
+        print("None")
+
+    def Count(self):
+        return self.iCount
+
+def main():
+    sobj = SinglyLL()
+
+    sobj.InsertFirst(101)
+    sobj.InsertFirst(51)
+    sobj.InsertFirst(21)
+    sobj.InsertFirst(11)
+
+    print("Elements of LinkedList are :")
+    sobj.Display()
+
+    print("Number of elements in LinkedList are : ",sobj.Count())
+
+    sobj.InsertLast(111)
+    sobj.InsertLast(121)
+
+    print("Elements of LinkedList are :")
+    sobj.Display()
+
+    print("Number of elements in LinkedList are : ",sobj.Count())
+
+    sobj.InsertAtPos(75,4)
+
+    print("Elements of LinkedList are :")
+    sobj.Display()
+
+    print("Number of elements in LinkedList are : ",sobj.Count())
+
+    sobj.DeleteFirst()
+    sobj.DeleteFirst()
+
+    print("Elements of LinkedList are :")
+    sobj.Display()
+
+    print("Number of elements in LinkedList are : ",sobj.Count())
+
+    sobj.DeleteLast()
+
+    print("Elements of LinkedList are :")
+    sobj.Display()
+
+    print("Number of elements in LinkedList are : ",sobj.Count())
+
+    sobj.DeleteAtPos(3)
+
+    print("Elements of LinkedList are :")
+    sobj.Display()
+
+    print("Number of elements in LinkedList are : ",sobj.Count())
+
+if __name__ == "__main__":
+    main()
+
